88c427b0fd331226d3b979d891850a0eb9e5f658
S.P <sergey.a.protsenko@mail.ru>
2022-05-15 15:57:26 +0700

    debug key deleted due to the presence in config.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ application.register_blueprint(directors_module)
 application.register_blueprint(genres_module)
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    application.run()
